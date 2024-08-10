b4afc66a3271c9461e9f6033e0cee4002f71ffcc
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-08-10 10:27:17 -0300

    FEAT: Create Default Layout and Default Components [Refs: #3]

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { DefaultHeader } from '@/components/default/DefaultHeader'
+import { DefaultFooter } from '@/components/default/DefaultFooter'
+import { RouterView } from 'vue-router';
+</script>
+
+<template>
+    <main>
+        <DefaultHeader />
+        <RouterView />
+        <DefaultFooter />
+    </main>
+</template>
+
+<style scoped>
+</style>
