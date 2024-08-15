2095cc25d98d04d294e5b8aeeca36bd795d54c4a
SPNicolas <nicolassteffenspereira@gmail.com>
2024-08-15 09:27:35 -0300

    FEAT: Started header links #6

--- src/components/default/DefaultHeader.vue
+++ src/components/default/DefaultHeader.vue
@@ -2,8 +2,39 @@
 </script>
 
 <template>
-  <header></header>
+  <header>
+    <div class="links-header">
+      <router-link class="link">Bolos</router-link>
+      <router-link class="link">Cafés</router-link>
+      <router-link class="link">Doces</router-link>
+      <router-link class="link">Pães</router-link>
+      <router-link class="link">Promoções</router-link>
+    </div>
+  </header>
+  
 </template>
 
 <style scoped>
+
+header {
+  width: 1440px;
+  height: 100px;
+  flex-shrink: 0;
+}
+
+.links-header {
+  height: 100px;
+  margin-top: 50px;
+}
+
+.link {
+  color: #000;
+  font-family: Inter;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 600;
+  line-height: normal;
+  text-decoration: none;
+}
+
 </style>
