e11a9ace1197165f4e9fd195d136639acef67c70
dnzLucas <lucasdefenix@gmail.com>
2024-08-17 00:22:18 -0300

    CHORE: Remove submit button to move to another branch. #11

--- src/components/ForgetPassword.vue
+++ src/components/ForgetPassword.vue
@@ -7,11 +7,16 @@
       <p>Digite seu email para receber um codigo de redefinição</p>
     </div>
     <input class="input-password" type="text" placeholder="Digite seu email" />
-    <input class="send-password" type="button" value="Enviar código">
   </div>
+
+  
 </template>
 
 <style>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
-@import '../assets/Sass/components/ResetPassword/ForgetPassword.scss';
+@import '../assets/Sass/components/ResetPassord/ForgetPassword.scss';
+
+.teste{
+  background-color: red;
+}
 </style>
