3136e63a97a111f9f3bf5011204ce525c13463b2
LuizBlume <luiz.fernando.251107@gmail.com>
2024-08-20 15:15:23 -0300

    CHORE: Update in SignIn #10.

--- src/views/SignInView.vue
+++ src/views/SignInView.vue
@@ -5,13 +5,13 @@
     <h1>Login</h1>
     <p>Preencha suas informações</p>
     <form>
-      <div class="form-group">
-        <label class="email" for="email">Email</label>
-        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Seu email">
+      <div class="form-group inputBox">
+        <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder=" ">
+        <label class="email labelInput" for="email">Email</label>
       </div>
-        <div class="form-group">
-            <label class="email" for="password">Senha</label>
-            <input type="password" class="form-control" id="password" placeholder="Senha">
+        <div class="form-group inputBox">
+          <input type="password" class="form-control input-user" id="password" placeholder=" ">
+          <label class="email labelInput" for="password">Senha</label>
         </div>
         <button type="submit" class="btn btn-primary">Entrar</button>
     </form>
