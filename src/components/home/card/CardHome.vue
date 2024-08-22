8b97911e7e4d8ff943194b69cafff1a3ced37ad7
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-08-22 19:24:44 -0300

    FEAT: Finish home [Refs: #8]

--- src/components/home/card/CardHome.vue
+++ src/components/home/card/CardHome.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { RouterLink } from 'vue-router';
 </script>
 
 <template>
@@ -6,18 +7,38 @@
         <div class="card">
             <h3>Bolos</h3>
             <p>ipsumlorem ipsumlorem ipsumlorem ipsumlorem</p>
+            <div class="container-button-card">
+                <router-link to="/">
+                    <button type="button">Ir a página <span><img src="@/assets/icons/vector.svg" alt=""></span></button>
+                </router-link>
+            </div>
         </div>
         <div class="card">
             <h3>Cafés</h3>
             <p>ipsumlorem ipsumlorem ipsumlorem ipsumlorem</p>
+            <div class="container-button-card">
+                <router-link to="/">
+                    <button type="button">Ir a página <span><img src="@/assets/icons/vector.svg" alt=""></span></button>
+                </router-link>
+            </div>
         </div>
         <div class="card">
             <h3>Doce</h3>
             <p>ipsumlorem ipsumlorem ipsumlorem ipsumlorem</p>
+            <div class="container-button-card">
+                <router-link>
+                    <button type="button" to="/">Ir a página <span><img src="@/assets/icons/vector.svg" alt=""></span></button>
+                </router-link>
+            </div>
         </div>
         <div class="card">
             <h3>Pães</h3>
             <p>ipsumlorem ipsumlorem ipsumlorem ipsumlorem</p>
+            <div class="container-button-card">
+                <router-link to="/">
+                    <button type="button">Ir a página <span><img src="@/assets/icons/vector.svg" alt=""></span></button>
+                </router-link>
+            </div>
         </div>
     </div>
 </template>
@@ -30,12 +51,40 @@
     margin: 50px 0 80px 0;
 }
 .card {
-    width: 200px;
-    height: 150px;
+    width: 250px;
+    height: 250px;
+    padding: 5px 20px;
     display: flex;
     flex-direction: column;
     align-items: center;
+    row-gap: 10px;
     border: 1px solid #000;
-    border-radius: 5px;
+    border-radius: 8px;
+}
+
+.card > h3 {
+    font-family: 'DM Serif Display', serif;
+    font-weight: 400;
+}
+
+.card > p {
+    flex-grow: 3;
+    font-family: 'Poppins', sans-serif;
+    text-align: center;
+}
+
+.container-button-card > a > button {
+    width: 175px;
+    height: 30px;
+    margin-bottom: 10px;
+    border: 1px solid #000;
+    border-radius: 15px;
+    background: transparent;
+    cursor: pointer;
+}
+
+.container-button-card > a > button > span {
+    position: relative;
+    left: 5px;
 }
 </style>
