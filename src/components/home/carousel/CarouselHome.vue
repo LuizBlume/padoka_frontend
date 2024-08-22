c63507e2a0da122131380a5d439648674e3adacb
Joaovictor23t4 <joaovictor239090@gmail.com>
2024-08-21 22:05:05 -0300

    CHORE: Fix some errors in carousel [Refs: #8]

--- src/components/home/carousel/CarouselHome.vue
+++ src/components/home/carousel/CarouselHome.vue
@@ -1,117 +1,134 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted } from 'vue';
 
-const cards = ref([1, 2, 3, 4, 5, 6, 7, 8])
-const innerStyles = ref({})
-const step = ref('')
-const transitioning = ref(false)
-const inner = ref(null)
+const carouselStyles = ref({});
+const cards = ref([1, 2, 3, 4]);
+const innerStyles = ref({});
+const transitioning = ref(false);
+const inner = ref(null);
+const currentIndex = ref(0);
 
-function setStep() {
-  const innerWidth = inner.value.scrollWidth
-  const totalCards = cards.value.length
-  step.value = `${innerWidth / totalCards}px`
-}
-
-function next() {
-  if (transitioning.value) return
-  transitioning.value = true
-
-  moveLeft()
-  afterTransition(() => {
-    const card = cards.value.shift()
-    cards.value.push(card)
-    resetTranslate()
-    transitioning.value = false
-  })
-}
-
-function prev() {
-  if (transitioning.value) return
-  transitioning.value = true
-
-  moveRight()
-  afterTransition(() => {
-    const card = cards.value.pop()
-    cards.value.unshift(card)
-    resetTranslate()
-    transitioning.value = false
-  })
-}
-
-function moveLeft() {
-  innerStyles.value = {
-    transform: `translateX(-${step.value}) translateX(-${step.value})`
+function action(index) {
+  if (index > currentIndex.value) {
+    next(Math.abs(index - currentIndex.value));
+  } else {
+    prev(Math.abs(currentIndex.value - index));
   }
+  currentIndex.value = index;
 }
 
-function moveRight() {
+function next(multiplier) {
+  if (transitioning.value) return;
+  transitioning.value = true;
+
+  moveLeft(multiplier);
+  afterTransition(() => {
+    resetTranslate();
+    transitioning.value = false;
+  });
+}
+
+function prev(multiplier) {
+  if (transitioning.value) return;
+  transitioning.value = true;
+
+  moveRight(multiplier);
+  afterTransition(() => {
+    resetTranslate();
+    transitioning.value = false;
+  });
+}
+
+function moveLeft(multiplier) {
   innerStyles.value = {
-    transform: `translateX(${step.value}) translateX(-${step.value})`
-  }
+    transform: `translateX(-${multiplier * 1724.55}px)`
+  };
+}
+
+function moveRight(multiplier) {
+  innerStyles.value = {
+    transform: `translateX(${multiplier * 1724.55}px)`
+  };
 }
 
 function afterTransition(callback) {
   const listener = () => {
-    callback()
-    inner.value.removeEventListener('transitionend', listener)
-  }
-  inner.value.addEventListener('transitionend', listener)
+    callback();
+    inner.value.removeEventListener('transitionend', listener);
+  };
+  inner.value.addEventListener('transitionend', listener);
 }
 
 function resetTranslate() {
   innerStyles.value = {
     transition: 'none',
-    transform: `translateX(-${step.value})`
-  }
+    transform: `translateX(-1724.55px)`
+  };
+  setTimeout(() => {
+    innerStyles.value.transition = 'transform 0.2s';
+  });
 }
 
 onMounted(() => {
-  setStep()
-  resetTranslate()
-})
+  resetTranslate();
+  carouselStyles.value = { width: `${document.documentElement.innerWidth}px` };
+});
 </script>
 
 <template>
-    <div class="carousel">
-        <div class="inner" ref="inner" :style="innerStyles">
-            <div class="card" v-for="card in cards" :key="card">
-                {{ card }}
-            </div>
-        </div>
+  <div class="carousel" :style="carouselStyles">
+    <div ref="inner" :style="innerStyles" class="inner">
+      <div v-for="card in cards" :key="card" class="card">
+        <img class="img-card" src="@/assets/images/carousel-img.jpg" alt="">
+      </div>
     </div>
-    <button @click="prev">prev</button>
-    <button @click="next">next</button>
+    <div class="buttons">
+      <button
+        v-for="(button, indexButton) in cards"
+        :key="indexButton"
+        class="button"
+        @click="action(indexButton)"
+      ></button>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .carousel {
-  width: 170px; /* ❶ */
-  overflow: hidden; /* ❷ */
+  display: flex;
+  overflow: hidden;
+  flex-direction: column;
+  align-items: center;
 }
 
 .inner {
-  transition: transform 0.2s;      
-  white-space: nowrap; /* ❸ */
+  transition: transform 0.2s;
+  white-space: nowrap;
 }
 
 .card {
-  width: 40px;
-  margin-right: 10px;
   display: inline-flex;
+  width: 100%;
+}
 
-  /* optional */
-  height: 40px;
-  background-color: #39b1bd;
-  color: white;
-  border-radius: 4px;
-  align-items: center;
-  justify-content: center;
+.img-card {
+  width: 100%;
+}
+
+.buttons {
+  display: flex;
+  position: relative;
+  column-gap: 20px;
+  bottom: 40px;
 }
 
 /* optional */
-button {
-  margin-right: 5px;
-  margin-top: 10px;
+.button {
+  background-color: #FDFDFD;
+  width: 15px;
+  height: 15px;
+  border-radius: 50%;
+  border: none;
+  outline: none;
 }
 </style>
