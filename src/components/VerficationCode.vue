2752f4df4fcd59c15610df648a6ec65d6ac79e75
dnzlucas <lucasdefenix@gmail.com>
2024-08-22 09:30:07 -0300

    CHORE: Add VerificationCode styles and titles #12

---
+++ src/components/VerficationCode.vue
@@ -0,0 +1,15 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="verification-container">
+        <h1>Digite o código de verificação</h1>
+        <p>Foi enviado o código de verificação para emailemailemail@gmail.com</p>
+    </div>
+</template>
+
+<style scoped>
+@import "../assets/Sass/components/VerificationCode/VerificationCode.scss";
+
+</style>
