13e58fcc14dc3b3841990d6c05cc92358aff5adb
SPNicolas <nicolassteffenspereira@gmail.com>
2024-08-22 00:06:12 -0300

    FEAT: Breads page finished #23

--- src/views/BreadsView.vue
+++ src/views/BreadsView.vue
@@ -4,11 +4,164 @@
 
 
 <template>
-  <div>Breads</div>
+  <div>
+    <div class="main-breads">
+      <h1>Pães</h1>
+      <div class="type-breads">
+        <div class="breads">
+          <img src="https://s3-alpha-sig.figma.com/img/aed2/5892/0bd711f194723fb894047a9dc23c3d57?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gG6~r5xZdCxSIqRNxypSsRG9gnatZsyoH8x7VSrP0SvNA3WhD7gzd9fPYzdVYbKn4WTtODo2HA-zLoZkxjDN5SsY8Om02vWpPVdKz12lWnizeAwqTRXAMpajEj89dWeixDRcMSdvTr62x5dKIWddIJZilu79F78AYsxpdR4pNcxLo6E-AJPrfhTTQ0bIjcbWa9wg7fCmbhOzz5VNRStfp-AYhv~rNkFXNGNjcn6OQczKJTHrK-jHDooTPaRel5s7Afc2jwtR2W-3mA4xbXijNlMMqSYqSOO6iSiYHmjFvhcMU7BaHLDicwpzhdr~5dp~O1aJRbupwgWAAb0GISUKXw__" alt="">
+          <h1>Pães</h1>
+          <p>Loremipsum Loremipsum</p>
+          <p>Loremipsum Loremipsum</p>
+          <div class="page">
+            <p>Ir A Página</p>
+            <img src="@/assets/icons/Vector.svg" class="icons" alt="">
+          </div>
+        </div>
+        <div class="breads">
+          <img src="https://s3-alpha-sig.figma.com/img/aed2/5892/0bd711f194723fb894047a9dc23c3d57?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gG6~r5xZdCxSIqRNxypSsRG9gnatZsyoH8x7VSrP0SvNA3WhD7gzd9fPYzdVYbKn4WTtODo2HA-zLoZkxjDN5SsY8Om02vWpPVdKz12lWnizeAwqTRXAMpajEj89dWeixDRcMSdvTr62x5dKIWddIJZilu79F78AYsxpdR4pNcxLo6E-AJPrfhTTQ0bIjcbWa9wg7fCmbhOzz5VNRStfp-AYhv~rNkFXNGNjcn6OQczKJTHrK-jHDooTPaRel5s7Afc2jwtR2W-3mA4xbXijNlMMqSYqSOO6iSiYHmjFvhcMU7BaHLDicwpzhdr~5dp~O1aJRbupwgWAAb0GISUKXw__" alt="">
+          <h1>Pães</h1>
+          <p>Loremipsum Loremipsum</p>
+          <p>Loremipsum Loremipsum</p>
+          <div class="page">
+            <p>Ir A Página</p>
+            <img src="@/assets/icons/Vector.svg" class="icons" alt="">
+          </div>
+        </div>
+        <div class="breads">
+          <img src="https://s3-alpha-sig.figma.com/img/aed2/5892/0bd711f194723fb894047a9dc23c3d57?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gG6~r5xZdCxSIqRNxypSsRG9gnatZsyoH8x7VSrP0SvNA3WhD7gzd9fPYzdVYbKn4WTtODo2HA-zLoZkxjDN5SsY8Om02vWpPVdKz12lWnizeAwqTRXAMpajEj89dWeixDRcMSdvTr62x5dKIWddIJZilu79F78AYsxpdR4pNcxLo6E-AJPrfhTTQ0bIjcbWa9wg7fCmbhOzz5VNRStfp-AYhv~rNkFXNGNjcn6OQczKJTHrK-jHDooTPaRel5s7Afc2jwtR2W-3mA4xbXijNlMMqSYqSOO6iSiYHmjFvhcMU7BaHLDicwpzhdr~5dp~O1aJRbupwgWAAb0GISUKXw__" alt="">
+          <h1>Pães</h1>
+          <p>Loremipsum Loremipsum</p>
+          <p>Loremipsum Loremipsum</p>
+          <div class="page">
+            <p>Ir A Página</p>
+            <img src="@/assets/icons/Vector.svg" class="icons" alt="">
+          </div>
+        </div>
+        <div class="breads">
+          <img src="https://s3-alpha-sig.figma.com/img/aed2/5892/0bd711f194723fb894047a9dc23c3d57?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gG6~r5xZdCxSIqRNxypSsRG9gnatZsyoH8x7VSrP0SvNA3WhD7gzd9fPYzdVYbKn4WTtODo2HA-zLoZkxjDN5SsY8Om02vWpPVdKz12lWnizeAwqTRXAMpajEj89dWeixDRcMSdvTr62x5dKIWddIJZilu79F78AYsxpdR4pNcxLo6E-AJPrfhTTQ0bIjcbWa9wg7fCmbhOzz5VNRStfp-AYhv~rNkFXNGNjcn6OQczKJTHrK-jHDooTPaRel5s7Afc2jwtR2W-3mA4xbXijNlMMqSYqSOO6iSiYHmjFvhcMU7BaHLDicwpzhdr~5dp~O1aJRbupwgWAAb0GISUKXw__" alt="">
+          <h1>Pães</h1>
+          <p>Loremipsum Loremipsum</p>
+          <p>Loremipsum Loremipsum</p>
+          <div class="page">
+            <p>Ir A Página</p>
+            <img src="@/assets/icons/Vector.svg" class="icons" alt="">
+          </div>
+        </div>
+      </div>
+      <div class="type-breads">
+        <div class="breads">
+          <img src="https://s3-alpha-sig.figma.com/img/aed2/5892/0bd711f194723fb894047a9dc23c3d57?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gG6~r5xZdCxSIqRNxypSsRG9gnatZsyoH8x7VSrP0SvNA3WhD7gzd9fPYzdVYbKn4WTtODo2HA-zLoZkxjDN5SsY8Om02vWpPVdKz12lWnizeAwqTRXAMpajEj89dWeixDRcMSdvTr62x5dKIWddIJZilu79F78AYsxpdR4pNcxLo6E-AJPrfhTTQ0bIjcbWa9wg7fCmbhOzz5VNRStfp-AYhv~rNkFXNGNjcn6OQczKJTHrK-jHDooTPaRel5s7Afc2jwtR2W-3mA4xbXijNlMMqSYqSOO6iSiYHmjFvhcMU7BaHLDicwpzhdr~5dp~O1aJRbupwgWAAb0GISUKXw__" alt="">
+          <h1>Pães</h1>
+          <p>Loremipsum Loremipsum</p>
+          <p>Loremipsum Loremipsum</p>
+          <div class="page">
+            <p>Ir A Página</p>
+            <img src="@/assets/icons/Vector.svg" class="icons" alt="">
+          </div>
+        </div>
+        <div class="breads">
+          <img src="https://s3-alpha-sig.figma.com/img/aed2/5892/0bd711f194723fb894047a9dc23c3d57?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gG6~r5xZdCxSIqRNxypSsRG9gnatZsyoH8x7VSrP0SvNA3WhD7gzd9fPYzdVYbKn4WTtODo2HA-zLoZkxjDN5SsY8Om02vWpPVdKz12lWnizeAwqTRXAMpajEj89dWeixDRcMSdvTr62x5dKIWddIJZilu79F78AYsxpdR4pNcxLo6E-AJPrfhTTQ0bIjcbWa9wg7fCmbhOzz5VNRStfp-AYhv~rNkFXNGNjcn6OQczKJTHrK-jHDooTPaRel5s7Afc2jwtR2W-3mA4xbXijNlMMqSYqSOO6iSiYHmjFvhcMU7BaHLDicwpzhdr~5dp~O1aJRbupwgWAAb0GISUKXw__" alt="">
+          <h1>Pães</h1>
+          <p>Loremipsum Loremipsum</p>
+          <p>Loremipsum Loremipsum</p>
+          <div class="page">
+            <p>Ir A Página</p>
+            <img src="@/assets/icons/Vector.svg" class="icons" alt="">
+          </div>
+        </div>
+        <div class="breads">
+          <img src="https://s3-alpha-sig.figma.com/img/aed2/5892/0bd711f194723fb894047a9dc23c3d57?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gG6~r5xZdCxSIqRNxypSsRG9gnatZsyoH8x7VSrP0SvNA3WhD7gzd9fPYzdVYbKn4WTtODo2HA-zLoZkxjDN5SsY8Om02vWpPVdKz12lWnizeAwqTRXAMpajEj89dWeixDRcMSdvTr62x5dKIWddIJZilu79F78AYsxpdR4pNcxLo6E-AJPrfhTTQ0bIjcbWa9wg7fCmbhOzz5VNRStfp-AYhv~rNkFXNGNjcn6OQczKJTHrK-jHDooTPaRel5s7Afc2jwtR2W-3mA4xbXijNlMMqSYqSOO6iSiYHmjFvhcMU7BaHLDicwpzhdr~5dp~O1aJRbupwgWAAb0GISUKXw__" alt="">
+          <h1>Pães</h1>
+          <p>Loremipsum Loremipsum</p>
+          <p>Loremipsum Loremipsum</p>
+          <div class="page">
+            <p>Ir A Página</p>
+            <img src="@/assets/icons/Vector.svg" class="icons" alt="">
+          </div>
+        </div>
+        <div class="breads">
+          <img src="https://s3-alpha-sig.figma.com/img/aed2/5892/0bd711f194723fb894047a9dc23c3d57?Expires=1725235200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gG6~r5xZdCxSIqRNxypSsRG9gnatZsyoH8x7VSrP0SvNA3WhD7gzd9fPYzdVYbKn4WTtODo2HA-zLoZkxjDN5SsY8Om02vWpPVdKz12lWnizeAwqTRXAMpajEj89dWeixDRcMSdvTr62x5dKIWddIJZilu79F78AYsxpdR4pNcxLo6E-AJPrfhTTQ0bIjcbWa9wg7fCmbhOzz5VNRStfp-AYhv~rNkFXNGNjcn6OQczKJTHrK-jHDooTPaRel5s7Afc2jwtR2W-3mA4xbXijNlMMqSYqSOO6iSiYHmjFvhcMU7BaHLDicwpzhdr~5dp~O1aJRbupwgWAAb0GISUKXw__" alt="">
+          <h1>Pães</h1>
+          <p>Loremipsum Loremipsum</p>
+          <p>Loremipsum Loremipsum</p>
+          <div class="page">
+            <p>Ir A Página</p>
+            <img src="@/assets/icons/Vector.svg" class="icons" alt="">
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 
 <style scoped>
+
+  .main-breads {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    gap: 60px;
+    flex-direction: column;
+  }
+
+  .type-breads {
+    display: flex;
+    width: 1200px;
+    height: 307px;
+    justify-content: center;
+    gap: 41px;
+  }
+
+  .breads {
+    width: 270px;
+    height: 308px;
+    flex-shrink: 0;
+    border: 1px solid black;
+    border-radius: 15px;
+    justify-content: center;
+    text-align: center;
+  }
+
+  .breads h1 {
+    color: #000;
+    font-family: Poppins;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  }
+
+  .breads p {
+    padding: 3px;
+  }
+
+  .breads img {
+    width: 268px;
+    height: 114px;
+    flex-shrink: 0;
+    border-radius: 15px 15px 0px 0px;
+  }
+
+  .page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 201.168px;
+    height: 34px;
+    flex-shrink: 0;
+    border-radius: 25px;
+    border: 1px solid #000;
+    background: #FFF;
+    margin: 20px auto 0; 
+  }
+
+  .page img {
+    width: 12.01px;
+    height: 12px;
+    flex-shrink: 0;
+  }
 
 </style>
 
