6f00c90051da799fa9e54c66b03e4803767022da
dnzlucas <lucasdefenix@gmail.com>
2024-08-23 14:47:55 -0300

    FEAT: Create NewPassword component and import Submit Button #13

---
+++ src/components/NewPassword.vue
@@ -0,0 +1,18 @@
+<script setup>
+import SubmitButton from './SubmitButton.vue'
+</script>
+
+<template>
+  <div class="newPassword-container">
+    <h1>Digite sua nova senha</h1>
+    <div class="inputs">
+      <input type="text" placeholder="Digite sua nova senha"/>
+      <input type="text" placeholder="Confirme sua senha"/>
+    </div>
+    <SubmitButton title="Enviar"/>
+  </div>
+</template>
+
+<style scoped>
+@import '../assets/Sass/components/NewPassword/_newPassword.scss';
+</style>
