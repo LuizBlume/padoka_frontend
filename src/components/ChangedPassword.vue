ab478b6e3a04784f93ed36440b7f4d7375b25122
dnzlucas <lucasdefenix@gmail.com>
2024-08-23 15:19:06 -0300

    FEAT: Add Chek image and create archive sass #14

---
+++ src/components/ChangedPassword.vue
@@ -0,0 +1,13 @@
+<script setup>
+import SubmitButton from './SubmitButton.vue';
+</script>
+
+<template>
+    <div class="changedPassword-container">
+        <img src="../assets/vector_288_02-512.png" alt="">
+    </div>
+</template>
+
+<style scoped>
+
+</style>
