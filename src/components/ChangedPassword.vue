230dad52f2fab03ba13b9b8b39acfd51ef25cf62
dnzLucas <lucasdefenix@gmail.com>
2024-08-23 22:59:59 -0300

    CHORE: Change image and import button #14

--- src/components/ChangedPassword.vue
+++ src/components/ChangedPassword.vue
@@ -4,10 +4,12 @@ import SubmitButton from './SubmitButton.vue';
 
 <template>
     <div class="changedPassword-container">
-        <img src="../assets/vector_288_02-512.png" alt="">
+        <img src="../assets/Eo_circle_green_checkmark.svg" alt="">
+        <h1>Senha atualizada com sucesso!</h1>
+        <SubmitButton title="Continuar"/>
     </div>
 </template>
 
 <style scoped>
-
+@import "../assets/Sass/components/ChangedPassword/_changedPassword.scss" ;
 </style>
