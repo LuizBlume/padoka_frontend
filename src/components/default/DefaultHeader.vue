e1e2d6a808a17d4028b04bd2b91a150cb338b5af
LuizBlume <luiz.fernando.251107@gmail.com>
2024-08-27 15:12:19 -0300

    REFACTOR: Updating in text and images #23.

--- src/components/default/DefaultHeader.vue
+++ src/components/default/DefaultHeader.vue
@@ -9,14 +9,17 @@ console.log(route.path)
   <header>
     <div class="container-header">
       <div class="links-header">
-        <router-link class="link">Bolos</router-link>
-        <router-link class="link">Cafés</router-link>
-        <router-link class="link">Doces</router-link>
-        <router-link class="link">Pães</router-link>
-        <router-link class="link">Promoções</router-link>
+        <router-link to="/cakes" class="link">Bolos</router-link>
+        <router-link to="/coffes" class="link">Cafés</router-link>
+        <router-link to="/candy" class="link">Doces</router-link>
+        <router-link to="/breads" class="link">Pães</router-link>
+        <router-link to="/promotions" class="link">Promoções</router-link>
+        <router-link to="/signin" class="link">Cadastro</router-link>
       </div>
       <div class="container-logo">
-        <img src="@/assets/logo.png" alt="">
+        <router-link to="/">
+          <img src="@/assets/logo.png" alt="">
+        </router-link>
       </div>
       <div class="nav-buttons">
         <div v-if="route.path !== '/stock' && route.path !== '/supplier-purchases'" class="container-search">
@@ -25,11 +28,15 @@ console.log(route.path)
         </div>
 
         <div class="container-profile">
-          <img src="@/assets/icons/profile.svg" class="icons" alt="">
+          <router-link to="/perfil">
+            <img src="@/assets/icons/profile.svg" class="icons" alt="">
+          </router-link>
         </div>
 
         <div class="container-shop">
-          <img src="@/assets/icons/shop.svg" class="icons" alt="">
+          <router-link to="/requests">
+            <img src="@/assets/icons/shop.svg" class="icons" alt="">
+          </router-link>
         </div>
       </div>
     </div>
@@ -37,7 +44,6 @@ console.log(route.path)
 </template>
 
 <style scoped>
-
 header {
   padding: 10px 0;
 }
@@ -53,7 +59,7 @@ header {
   column-gap: 30px;
 }
 
-.links-header > a {
+.links-header>a {
   font-family: 'Poppins', sans-serif;
 }
 
@@ -96,10 +102,12 @@ input[type="search"]::-webkit-search-decoration,
 input[type="search"]::-webkit-search-cancel-button,
 input[type="search"]::-webkit-search-results-button,
 input[type="search"]::-webkit-search-results-decoration {
-  display: none; 
+  display: none;
 }
 
-.container-search, .container-profile, .container-shop {
+.container-search,
+.container-profile,
+.container-shop {
   padding: 0 10px;
 }
 
